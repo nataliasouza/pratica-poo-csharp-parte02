ce7b709d07df0a1c37abb9d9b7648f4cbe9c73a1
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-08-16 10:51:17 -0300

    Central de Exercício - Estrutura básica para exercícios

---
+++ CursoParte02/CursoParte02/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using CursoParte02;
+using CursoParte02.Secao10;
+
+var central = new CentralDeExercicios(new Dictionary<string, Action>()
+{
+    #region "Seção 10 Herança"
+
+    // PROGRAMAÇÃO ORIENTADA A OBJETOS - Seção 10
+    // Capítulo: Herança e Polimorfismo
+
+    {"Seção 10 - Exercício Herança", Heranca.Executar},
+
+
+    #endregion
+
+});
+
+central.SelecionarEExecutar();
---
+++ CursoParte02/CursoParte02/Secao10/Heranca.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoParte02.Secao10;
+
+internal class Heranca
+{
+    public static void Executar()
+    {
+
+    }
+}
