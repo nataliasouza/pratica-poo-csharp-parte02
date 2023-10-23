88585809e54bf03240c64a81a7f174c4e2221f91
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-23 15:27:23 -0300

    Seção 15 - Exercício Conjuntos - HashSet e SortedSet Parte 1 - Aula 217

---
+++ CursoParte02/CursoParte02/Section15/ExerciseSet/ExerciseHashSet.cs
@@ -0,0 +1,25 @@
+﻿namespace CursoParte02.Section15.ExerciseSet
+{
+    internal class ExerciseHashSet
+    {
+        public static void Executar()
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");            
+
+            Console.WriteLine("\nItems do conjunto: \n");
+
+            foreach (string item in set) 
+            { 
+                Console.WriteLine($"{item}" ); 
+            }
+
+            Console.WriteLine("\n\nExiste no conjunto a palavra: Notebook");
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine("\nExiste no conjunto a palavra: Mouse");
+            Console.WriteLine(set.Contains("Mouse"));
+        }
+    }
+}
